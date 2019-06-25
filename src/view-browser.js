56074261505e334a6cca0e268642cfaf16c2bc70
Valentin Rybin <valentine.rybin@gmail.com>
2019-06-25 22:07:12 +0300

    Fix Ctrl+F interception
    This handler prevents default browser search action.
    This handler is useless, as it fails with error.

--- src/view-browser.js
+++ src/view-browser.js
@@ -389,10 +389,6 @@ host.BrowserHost = class {
     _keyHandler(e) {
         if (!e.altKey && !e.shiftKey && (e.ctrlKey || e.metaKey)) {
             switch (e.keyCode) {
-                case 70: // F
-                    this._view.find();
-                    e.preventDefault();
-                    break;
                 case 68: // D
                     this._view.toggleAttributes();
                     e.preventDefault();
